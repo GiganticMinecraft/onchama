47f5835e681bf91dd7dd1d3a5f1415777cc67923
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-09-25 23:47:00 +0900

    feat: add Data to share something

--- src/shared/data.rs
+++ src/shared/data.rs
@@ -1,4 +1,29 @@
 use derive_new::new;
+use once_cell::sync::Lazy;
+use poise::serenity_prelude::ChannelId;
+use std::sync::{Arc, Mutex};
+
+#[derive(new, Default)]
+pub struct DataModel<T: Default + Clone>(#[new(default)] Lazy<Arc<Mutex<Option<T>>>>);
+
+impl<T: Default + Clone> DataModel<T> {
+    pub fn save(&self, value: T) {
+        let mut lock = self.0.lock().unwrap();
+        *lock = Some(value)
+    }
+
+    pub fn clear(&self) {
+        let mut lock = self.0.lock().unwrap();
+        *lock = None
+    }
+
+    pub fn get(&self) -> Option<T> {
+        self.0.lock().unwrap().clone()
+    }
+}
 
 #[derive(new)]
-pub struct Data {}
+pub struct Data {
+    #[new(default)]
+    pub text_ch: DataModel<ChannelId>,
+}
